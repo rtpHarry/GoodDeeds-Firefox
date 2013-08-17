036abe18ffc17b4592f0a61f30a339932e559726
rtpHarry <rtpharry@hotmail.com>
2013-08-18 00:40:14 +0100

    enabled date check ready for v1.1 release

--- good-deeds-v1.1@rtpsoftware.co.uk/chrome/content/good-deeds.js
+++ good-deeds-v1.1@rtpsoftware.co.uk/chrome/content/good-deeds.js
@@ -93,7 +93,7 @@ var GoodDeeds = {
 	decide_what_to_display: function()
 	{
 		// check if tabs opened today
-		if(true /*this.compare_date()*/)
+		if(this.compare_date())
 		{
 			// open tabs
 			this.open_tabs();
@@ -101,7 +101,6 @@ var GoodDeeds = {
 			// update last run
 			var Today = new Date;
 			this.prefs.setCharPref("lastVisited", Today.toString());
-
 		}
 	},
 	
